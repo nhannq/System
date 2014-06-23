5781e57c0b8e17e40c8a4b3c2c3d79e67d236ead
nhannq <nhannq88@gmail.com>
2014-06-23 15:00:43 -0400

    add .

--- Scripts/getCassandraClass.py
+++ Scripts/getCassandraClass.py
@@ -8,7 +8,7 @@ for dir in directories:
 		f.write("------------" + dir + "-----------\n")
 		classes = os.listdir(dir)
 		for i in range(len(classes)-1):
-			f.write("org.apache.cassandra" + dir + classes[i].strip(".java") + ", ")
-		f.write(classes[len(classes)-1] + "\n")
+			f.write("org.apache.cassandra." + dir + "." + classes[i].strip(".java") + ", ")
+		f.write("org.apache.cassandra." + dir + "." + classes[len(classes)-1].strip(".java") + "\n")
 f.close()
 
