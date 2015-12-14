b27e3ec66a319a3b28c4307c80adacaca10e861c
nn <nn@n.n>
2015-12-14 15:29:08 -0500

    add hadooplogparser

---
+++ hadooplogparser/runCount.sh
@@ -0,0 +1,4 @@
+echo 24
+/home/nqn12001/hadooplog/cmd/count24.sh logs/userlogs/application_1448952291292_0001 > tmpCount/rs24_01
+echo 24
+/home/nqn12001/hadooplog/cmd/count24.sh logs/userlogs/application_1448952291292_0002 > tmpCount/rs24_02
